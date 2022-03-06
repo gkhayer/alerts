831c923ee547349c9fccd71a037f4802e6278447
Rupinder Hayer <25388689+rupinderrr@users.noreply.github.com>
2022-03-06 02:54:58 -0800

    Added alertcomponent to display alerts, met all the requirements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
 import { Container } from "@mui/material";
 import { AlertExample } from "./components/AlertExample";
 import { AlertManager } from "./components/AlertManager";
+import { useAlertReducer } from "./components/AlertManager";
 
 function App() {
   const [state, dispatch] = React.useReducer(useAlertReducer, []);
--- src/components/AlertManager.js
+++ src/components/AlertManager.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Stack } from "@mui/material";
+import { AlertComponent } from "./AlertComponent";
 
 export const useAlertReducer = (state, action) => {
   switch (action.type) {
@@ -11,6 +13,20 @@ export const useAlertReducer = (state, action) => {
   }
 };
 export const AlertManager = (props) => {
-  console.log(props.state);
-  return <></>;
+  return (
+    <Stack
+      sx={{
+        top: 0,
+        right: 0,
+        position: "absolute",
+        width: "30%",
+        m: ".5rem",
+      }}
+      spacing={1}
+    >
+      {props.state?.map((alert) => (
+        <AlertComponent key={alert.id} id={alert.id} alert={alert} />
+      ))}
+    </Stack>
+  );
 };
